a3fadf018f2b7a947ee830aec3f6f9fdbb6f6ed5
redpangilinan <redpangilinan715@gmail.com>
2023-07-23 04:11:55 +0800

    refactor: change naming from contents config

--- components/pages/hero.tsx
+++ components/pages/hero.tsx
@@ -2,46 +2,49 @@ import Link from "next/link"
 import Image from "next/image"
 import { buttonVariants } from "@/components/ui/button"
 import { cn } from "@/lib/utils"
-import { header } from "@/config/contents"
+import { heroHeader } from "@/config/contents"
+import { layout } from "@/config/layout"
 
-export default function Hero() {
-  let layout, direction
-  if (header.layout === "right") {
-    layout = "md:flex-row-reverse md:items-center"
+export default function HeroHeader() {
+  let format, direction
+  if (layout.heroHeader === "right") {
+    format = "md:flex-row-reverse md:items-center"
     direction = "md:text-left"
-  } else if (header.layout === "center") {
-    layout = "md:flex-col-reverse md:text-center md:items-center"
+  } else if (layout.heroHeader === "center") {
+    format = "md:flex-col-reverse md:text-center md:items-center"
     direction = "md:text-center md:items-center"
   } else {
-    layout = "md:flex-row md:items-center"
+    format = "md:flex-row md:items-center"
     direction = "md:text-left md:justify-start"
   }
 
   return (
     <header className="md:container">
       <div
-        className={`flex flex-col-reverse gap-8 px-8 py-16 text-center md:items-start md:px-0 lg:py-32 ${layout}`}
+        className={`flex flex-col-reverse gap-8 px-8 py-16 text-center md:items-start md:px-0 lg:py-28 ${format}`}
       >
         <div
           className={`flex flex-1 flex-col items-center gap-8 text-center md:items-start ${direction}`}
         >
           <div className="space-y-2">
-            <h1 className="text-4xl font-bold">{header.content.headline}</h1>
+            <h1 className="text-4xl font-bold md:text-5xl">
+              {heroHeader.headline}
+            </h1>
             <h2 className="text-lg font-light text-muted-foreground lg:text-xl">
-              {header.content.subheader}
+              {heroHeader.subheader}
             </h2>
           </div>
           <Link
-            href="/contact"
+            href={heroHeader.button.route}
             className={`w-[8rem] ${cn(buttonVariants({ size: "sm" }))}`}
           >
-            Get Started
+            {heroHeader.button.text}
           </Link>
         </div>
-        {header.content.image !== "" ? (
+        {heroHeader.image !== "" ? (
           <div className="flex flex-1 justify-center">
             <Image
-              src={header.content.image}
+              src={heroHeader.image}
               width={600}
               height={600}
               alt="Header image"
