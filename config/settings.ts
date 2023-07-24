cf1b1c85e3c4af602b6481bc16ecb56924f7d50e
redpangilinan <redpangilinan715@gmail.com>
2023-07-24 20:24:24 +0800

    feat: add comment to settings.ts

--- config/settings.ts
+++ config/settings.ts
@@ -1,5 +1,10 @@
 import { Settings } from "@/types"
 
+/* ====================
+[> WEBSITE SETTINGS <]
+-- Enable certain features by selecting true or false
+ ==================== */
+
 export const settings: Settings = {
   themeToggleEnabled: true,
 }
