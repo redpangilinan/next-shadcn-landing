d4122dca5ba40abb03d0fd73280d38062c055958
redpangilinan <redpangilinan715@gmail.com>
2023-07-24 20:23:20 +0800

    refactor: remove layout configuration

--- components/pages/feature-cards.tsx
+++ components/pages/feature-cards.tsx
@@ -1,7 +1,6 @@
 import Image from "next/image"
 import HeadingText from "@/components/heading-text"
 import { featureCards } from "@/config/contents"
-import { layout } from "@/config/layout"
 import {
   Card,
   CardContent,
@@ -11,33 +10,24 @@ import {
 } from "@/components/ui/card"
 
 export default function FeatureCards() {
-  let direction
-  if (layout.headers.featureCards === "right") {
-    direction = "text-right"
-  } else if (layout.headers.featureCards === "left") {
-    direction = "text-left"
-  } else {
-    direction = "text-center"
-  }
-
   return (
     <section className="space-y-8 px-8 py-16 text-center md:items-start md:px-0 lg:py-20">
-      <HeadingText subtext={featureCards.subheader} className={direction}>
+      <HeadingText subtext={featureCards.subheader} className="center">
         {featureCards.header}
       </HeadingText>
       <div className="flex flex-wrap justify-center gap-4">
         {featureCards.content.map((cards) => (
           <Card
             key={cards.text}
-            className="flex flex-grow flex-col justify-between"
+            className="flex flex-grow flex-col justify-between text-left"
           >
             <CardHeader>
               {cards.image !== "" ? (
-                <div className="flex flex-1 justify-center">
+                <div className="flex flex-1">
                   <Image
                     src={cards.image}
-                    width={150}
-                    height={150}
+                    width={100}
+                    height={100}
                     alt="Card image"
                   />
                 </div>
--- components/pages/hero.tsx
+++ components/pages/hero.tsx
@@ -3,42 +3,29 @@ import Image from "next/image"
 import { buttonVariants } from "@/components/ui/button"
 import { cn } from "@/lib/utils"
 import { heroHeader } from "@/config/contents"
-import { layout } from "@/config/layout"
 
 export default function HeroHeader() {
-  let format, direction
-  if (layout.heroHeader === "right") {
-    format = "md:flex-row-reverse md:items-center"
-    direction = "md:text-left"
-  } else if (layout.heroHeader === "center") {
-    format = "md:flex-col-reverse md:text-center md:items-center"
-    direction = "md:text-center md:items-center"
-  } else {
-    format = "md:flex-row md:items-center"
-    direction = "md:text-left md:justify-start"
-  }
-
   return (
     <header className="md:container">
       <div
-        className={`flex flex-col-reverse gap-8 px-8 py-16 text-center md:items-start md:px-0 lg:py-28 ${format}`}
+        className={`flex flex-col-reverse gap-8 px-8 py-16 text-center md:flex-row md:items-center md:px-0 lg:py-28`}
       >
         <div
-          className={`flex flex-1 flex-col items-center gap-8 text-center md:items-start ${direction}`}
+          className={`flex flex-1 flex-col items-center gap-8 text-center md:items-start md:justify-start md:text-left`}
         >
           <div className="space-y-2">
             <h1 className="text-4xl font-bold md:text-5xl">
-              {heroHeader.headline}
+              {heroHeader.header}
             </h1>
             <h2 className="text-lg font-light text-muted-foreground lg:text-xl">
               {heroHeader.subheader}
             </h2>
           </div>
           <Link
-            href={heroHeader.button.route}
+            href="#features"
             className={`w-[8rem] ${cn(buttonVariants({ size: "sm" }))}`}
           >
-            {heroHeader.button.text}
+            Get started
           </Link>
         </div>
         {heroHeader.image !== "" ? (
