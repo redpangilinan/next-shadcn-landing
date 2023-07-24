d4d2718801e4e7dd05edb30fba55fdd834a3c710
redpangilinan <redpangilinan715@gmail.com>
2023-07-24 21:00:27 +0800

    refactor: classname fix

--- components/pages/hero.tsx
+++ components/pages/hero.tsx
@@ -7,12 +7,8 @@ import { heroHeader } from "@/config/contents"
 export default function HeroHeader() {
   return (
     <header className="md:container">
-      <div
-        className={`flex flex-col-reverse gap-8 px-8 py-16 text-center md:flex-row md:items-center md:px-0 lg:py-28`}
-      >
-        <div
-          className={`flex flex-1 flex-col items-center gap-8 text-center md:items-start md:justify-start md:text-left`}
-        >
+      <div className="flex flex-col-reverse gap-8 px-8 py-16 text-center md:flex-row md:items-center md:px-0 lg:py-28">
+        <div className="flex flex-1 flex-col items-center gap-8 text-center md:items-start md:justify-start md:text-left">
           <div className="space-y-2">
             <h1 className="text-4xl font-bold md:text-5xl">
               {heroHeader.header}
