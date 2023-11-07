f5a30b94e5f9d48f136b3546bd3a5e78fbbee813
redpangilinan <redpangilinan715@gmail.com>
2023-11-07 22:04:19 +0800

    feat: create reusable icons component

---
+++ components/icons.tsx
@@ -0,0 +1,105 @@
+import Image from "next/image"
+import {
+  BsMoonStars,
+  BsSun,
+  BsChevronLeft,
+  BsChevronRight,
+  BsChevronUp,
+  BsChevronDown,
+  BsFire,
+  BsCheck2,
+} from "react-icons/bs"
+import {
+  AiOutlineEllipsis,
+  AiOutlineWarning,
+  AiOutlinePlus,
+  AiOutlineClose,
+  AiOutlineFileSearch,
+} from "react-icons/ai"
+import { MdDeleteForever, MdOutlineLogout } from "react-icons/md"
+import { BiHistory, BiCalendar, BiBarChartAlt } from "react-icons/bi"
+import { FaUserAlt, FaSort } from "react-icons/fa"
+import { ImSpinner8, ImStatsBars } from "react-icons/im"
+import { RxMixerHorizontal } from "react-icons/rx"
+import { LuSettings } from "react-icons/lu"
+
+export type IconKeys = keyof typeof icons
+
+type IconsType = {
+  [key in IconKeys]: React.ElementType
+}
+
+// ** Custom svg or images can be used as icons by returning a JSX **
+const icons = {
+  // Custom icons
+  blank: () => {
+    return <></>
+  },
+  nextjs: () => {
+    return (
+      <Image
+        src="next.svg"
+        className="dark:brightness-0 dark:invert-[1]"
+        width={100}
+        height={100}
+        alt="Card image"
+      />
+    )
+  },
+  shadcnUi: () => {
+    return (
+      <Image
+        src="shadcn-ui.svg"
+        className="dark:brightness-0 dark:invert-[1]"
+        width={100}
+        height={100}
+        alt="Card image"
+      />
+    )
+  },
+  vercel: () => {
+    return (
+      <Image
+        src="vercel.svg"
+        className="dark:brightness-0 dark:invert-[1]"
+        width={100}
+        height={100}
+        alt="Card image"
+      />
+    )
+  },
+
+  // Features
+  fileSearch: AiOutlineFileSearch,
+  barChart: BiBarChartAlt,
+  settings: LuSettings,
+
+  // Mode Toggle
+  moon: BsMoonStars,
+  sun: BsSun,
+
+  // Navigation
+  back: BsChevronLeft,
+  next: BsChevronRight,
+  up: BsChevronUp,
+  down: BsChevronDown,
+  close: AiOutlineClose,
+
+  // Common
+  trash: MdDeleteForever,
+  spinner: ImSpinner8,
+  userAlt: FaUserAlt,
+  ellipsis: AiOutlineEllipsis,
+  warning: AiOutlineWarning,
+  add: AiOutlinePlus,
+  history: BiHistory,
+  signout: MdOutlineLogout,
+  calendar: BiCalendar,
+  sort: FaSort,
+  fire: BsFire,
+  statsBar: ImStatsBars,
+  mixer: RxMixerHorizontal,
+  check: BsCheck2,
+}
+
+export const Icons: IconsType = icons
